c9656af61d889e709c0f021608a66dcb8759082a
hackslab <n.abdulaziz0207@gmail.com>
2026-01-17 17:28:21 +0500

    feat: Add initial Home page integrating Header, AboutUs, Services, Portfolio, and SuccessNumbers components.

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
     <main>
       <Header />
       <AboutUs />
-      <SuccessNumbers />
       <ServicesSection />
       <PortfolioSection />
+      <SuccessNumbers />
     </main>
   );
 }
